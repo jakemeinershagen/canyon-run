00359a0b56345c204e6405b2437f668b7b737dbd
TheMinesAreShakin <85915007+TheMinesAreShakin@users.noreply.github.com>
2022-12-11 17:00:10 -0600

    basic player movement done

---
+++ src/Entities/player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mousePosition: Vector2 = get_viewport().get_mouse_position()
+	
+	$".".global_position.y = mousePosition.y
