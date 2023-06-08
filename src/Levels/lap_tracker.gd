b7376e08db7c7ca292a43e37a554f43e1b4713de
TheMinesAreShakin <85915007+TheMinesAreShakin@users.noreply.github.com>
2023-06-07 19:28:43 -0500

    less stupid fix for lap tracker #2

--- src/Levels/lap_tracker.gd
+++ src/Levels/lap_tracker.gd
@@ -4,5 +4,5 @@ extends Area2D
 func _on_LapTracker_body_entered(body: Node) -> void:
 	#TODO: figure out if there is a better way to prevent tilemap from
 	# colliding with lap tracker
-	if body.global_position.x < position.x && body.get_name() != "TileMap":
+	if body.global_position.x < position.x && body.get_name() == "Player":
 		RaceData.add_lap()
